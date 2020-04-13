14805ae9b805453f4ea58144b1fec31fb2c236f6
Shivesh Abhishek <shankeyshivesh@gmail.com>
2020-04-14 04:21:27 +0530

    [Test]sanity-check path test
    Signed-off-by: Shivesh Abhishek <shankeyshivesh@gmail.com>

--- stages/director-install-and-upgrade/components-health-check.sh
+++ stages/director-install-and-upgrade/components-health-check.sh
@@ -14,11 +14,11 @@ bash utils/e2e-cr jobname:components-health-check jobphase:Running
 echo -e "\n********* [ Making logs directory ] **********\n";
 mkdir -pv logs/basic-sanity-tests
 
-echo -e "Curent path:"
-pwd
+echo -e "Curent path: pwd"
+
 
 echo -e "\n************************ Running basic-sanity tests ***********************************\n"
-bash director-health-check.sh
+bash oep/scripts/director-health-check.sh
 
 # If any of the above check fails, then fail this job
 if [ $(cat oep/scripts/result.txt | grep -ic fail) != 0 ];then
