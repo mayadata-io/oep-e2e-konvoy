13a0f5406584e053c8505d5f98d8d67bb5de3bb0
Shivesh Abhishek <shankeyshivesh@gmail.com>
2020-04-16 00:11:01 +0530

    Add comppleted jobphase in components-health-check
    Signed-off-by: Shivesh Abhishek <shankeyshivesh@gmail.com>

--- stages/director-install-and-upgrade/components-health-check.sh
+++ stages/director-install-and-upgrade/components-health-check.sh
@@ -15,8 +15,8 @@ bash oep-e2e/scripts/director-health-check.sh
 
 # If any of the above check fails, then fail this job
 if [ $(cat result.txt | grep -ic fail) != 0 ];then
-  exit 1
   bash utils/e2e-cr jobname:components-health-check jobphase:Completed
+  exit 1
 else
   bash utils/e2e-cr jobname:components-health-check jobphase:Completed
 fi
