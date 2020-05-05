b89692dac2eee776dde189eeca1280878ee72d6e
Shivesh Abhishek <shankeyshivesh@gmail.com>
2020-05-05 12:40:30 +0530

    add director-gui check
    Signed-off-by: Shivesh Abhishek <shankeyshivesh@gmail.com>

---
+++ stages/selenium-grid/create-grid.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+pod() {
+  echo -e "\n*************Setting up Selenium Grid****************\n"
+  sshpass -p $pass ssh -o StrictHostKeyChecking=no $user@$ip -p $port 'cd oep-e2e-konvoy && bash stages/selenium-grid/create-grid.sh node '"'$CI_PIPELINE_ID'"''
+}
+
+node() {
+  bash utils/e2e-cr jobname:selenium-grid-deploy jobphase:Waiting
+  bash utils/e2e-cr jobname:selenium-grid-deploy jobphase:Running
+  bash utils/e2e-cr jobname:selenium-grid-cleanup jobphase:Waiting
+
+  PIPELINE_ID=$1
+
+  path=$(pwd)
+
+  echo -e "\n[ Cloning gui-automation repo ] ------------------------------------\n"
+  git clone https://github.com/mayadata-io/gui-automation.git
+  cd gui-automation
+  tests_count=`find . -type f -name '*_test.py' -exec grep -e 'def test_' '{}' \; | wc -l`
+  echo "Number of GUI test scripts: $tests_count"
+  cd ..
+
+  cd stages/director-gui/templates
+  ls
+  cluster1=$(echo "pipeline-$PIPELINE_ID")
+
+  echo -e "\n[ Creating selenium stack ] ----------------------------------------\n"
+  aws cloudformation create-stack --stack-name konvoy-selenium-grid-${cluster1} --template-body file://hub.yml --parameters ParameterKey=NumberOfChromeNodes,ParameterValue=$tests_count ParameterKey=ClusterName,ParameterValue=${cluster1} ParameterKey=LogName,ParameterValue=${cluster1}
+
+  cd ../../..
+  bash utils/e2e-cr jobname:selenium-grid-deploy jobphase:Completed
+}
+
+if [ "$1" == "node" ];then
+  node $2
+else
+  pod
+fi
---
+++ stages/selenium-grid/delete-grid.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -e
+pod() {
+  ## Installing OpenEBS using DOP on cluster2
+  echo -e "\n*************Cleaning up Selenium Grid****************\n"
+  sshpass -p $pass ssh -o StrictHostKeyChecking=no $user@$ip -p $port 'cd oep-e2e-konvoy && bash stages/selenium-grid/delete-grid.sh node '"'$CI_PIPELINE_ID'"''
+}
+
+node() {
+
+  bash utils/pooling jobname:selenium-grid-deploy
+  bash utils/e2e-cr jobname:selenium-grid-cleanup jobphase:Running
+
+  ls aws/
+  PIPELINE_ID=$1
+
+  {
+    cluster1=$(echo "pipeline-$PIPELINE_ID")
+    aws cloudformation delete-stack --stack-name konvoy-selenium-grid-${cluster1}
+  } || {
+    echo 'Selenium CloudFormation stack was absent'
+  }
+
+  bash utils/e2e-cr jobname:selenium-grid-cleanup jobphase:Completed
+}
+
+if [ "$1" == "node" ];then
+  node $2
+else
+  pod
+fi
