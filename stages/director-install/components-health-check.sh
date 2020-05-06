125078038ee202fcca9cfe54b9350de98cdcc08f
Shivesh Abhishek <shankeyshivesh@gmail.com>
2020-05-06 14:30:15 +0530

    update namespace for ingress-nginx check
    Signed-off-by: Shivesh Abhishek <shankeyshivesh@gmail.com>

--- stages/director-install/components-health-check.sh
+++ stages/director-install/components-health-check.sh
@@ -7,9 +7,22 @@
 echo -e "\n************************ Running basic-sanity tests ***********************************\n"
 bash oep-e2e/scripts/director-health-check.sh
 
-# bash utils/e2e-cr jobname:components-health-check jobphase:Completed
+  # Replace namespace for ingress-nginx check
+  sed -i 's/value: ingress-nginx/value: default/g' oep-e2e/litmus/director/ingress-nginx/run_litmus_test.yml
 
-# If any of the above check fails, then fail this job
-if [ $(cat result.txt | grep -ic fail) != 0 ];then
-  exit 1
+  echo -e "\n************************ Running basic-sanity tests ***********************************\n"
+  bash oep-e2e/scripts/director-health-check.sh
+
+  bash utils/e2e-cr jobname:components-health-check jobphase:Completed
+
+  # If any of the above check fails, then fail this job
+  if [ $(cat result.txt | grep -ic fail) != 0 ];then
+    exit 1
+  fi
+}
+
+if [ "$1" == "node" ];then
+  node
+else
+  pod
 fi
