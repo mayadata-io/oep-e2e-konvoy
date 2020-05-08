b9f1802d162be55a6ca54992fe2c3a3b78e690d2
Shivesh Abhishek <shankeyshivesh@gmail.com>
2020-05-08 10:11:59 +0530

    Add wait to client-components-check
    Signed-off-by: Shivesh Abhishek <shankeyshivesh@gmail.com>

--- stages/functional-testing-with-rest/cluster-connect-checks/client-components-check.sh
+++ stages/functional-testing-with-rest/cluster-connect-checks/client-components-check.sh
@@ -17,6 +17,9 @@ node() {
   # Verify current context
   kubectl config current-context
 
+  echo -e "\n Manual wait of 60s"
+  sleep 60
+
   echo "Check cluster client components-------------------------------------------------"
   kubectl create -f oep-e2e/litmus/director/cluster-connect-check/run_litmus_test.yml
 
